9e4466f38b46e2941af5f37f1b9dab9b946ef4c9
RoadtoFire <fire.jawwad@gmail.com>
2026-01-03 23:45:47 +0500

    Switch to PostgreSQL

--- MuslimDua/settings.py
+++ MuslimDua/settings.py
@@ -68,10 +68,10 @@ WSGI_APPLICATION = 'MuslimDua.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # If there is a DATABASE_URL environment variable (which Railway/Render provides), use it.
